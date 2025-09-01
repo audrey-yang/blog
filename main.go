1c576ef3c40b338f5eb9d212811cb0f61b6d0d72
Audrey Yang <ayang.penn@gmail.com>
2025-08-31 22:31:20 -0500

    Read data from file and add routing

--- main.go
+++ main.go
@@ -1,37 +1,87 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 )
-
-type Page struct {
-	Title string
-	Body  string
-}
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "HELLO\n")
 }
 
+type Post struct {
+	ID      string `json:"id"`
+	Title   string `json:"title"`
+	Summary string `json:"summary"`
+	Body    string `json:"body"`
+}
+
+type PostData struct {
+	Posts []Post `json:"posts"`
+}
+
+var posts []Post
+
+func readPostsFromFile() {
+	if len(posts) > 0 {
+		return
+	}
+
+	dataFile := "./posts.json"
+	dat, err := os.ReadFile(dataFile)
+	if err != nil {
+		panic(err)
+	}
+
+	var jsonData PostData
+	if err := json.Unmarshal(dat, &jsonData); err != nil {
+		panic(err)
+	}
+	posts = append(posts, jsonData.Posts...)
+}
+
 func index(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("index")
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	data := Page{
-		Title: "My first blog post",
-		Body:  "Hello, world!",
+	readPostsFromFile()
+	tmpl.Execute(w, posts)
+}
+
+func post(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("post", req.PathValue("id"))
+	tmpl, err := template.ParseFiles("templates/post.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	tmpl.Execute(w, data)
+	readPostsFromFile()
+	id := req.PathValue("id")
+	for _, post := range posts {
+		if post.ID == id {
+			tmpl.Execute(w, post)
+			return
+		}
+	}
 }
 
 func main() {
-	http.HandleFunc("/", index)
-	http.HandleFunc("/hello", hello)
-	http.ListenAndServe(":8088", nil)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/post/{id}", post)
+	mux.HandleFunc("/hello", hello)
+
+	fmt.Println("Listening...")
+	if err := http.ListenAndServe(":8090", mux); err != nil {
+		fmt.Printf("Server failed to start: %v\n", err)
+	}
 }
